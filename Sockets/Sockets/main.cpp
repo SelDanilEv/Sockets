ebb8a184de8f3f8b69fdcaeb9421160105722bd9
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2020-10-01 13:13:12 +0300

    fix: fix gitignore

--- Sockets/Sockets/main.cpp
+++ Sockets/Sockets/main.cpp
@@ -3,27 +3,10 @@
 int main()
 {
     SocketUtil::StaticInit();
-    //TCPSocketPtr serverSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-
-    //SOCKADDR_IN addr;
     SocketAddressPtr servAddr = IPAddressFactory::CreateIPv4FromString("192.168.43.112:2000");
 
     SocketAddress* inAddress = new SocketAddress(servAddr->mSockAddr);
 
-    //serverSocket->Bind(*inAddress);
-
-    //serverSocket->Listen(1);
-
-    //serverSocket->Accept(*inAddress);
-
-    //char sibuf[50],
-    //    sobuf[50] = "sever: hello";
-
-/*    serverSocket->Receive(sibuf, sizeof(sibuf));
-    std::cout << sibuf;
-    serverSocket->Send(sobuf, sizeof(sobuf))*/;
-
-    //client
     TCPSocketPtr clientSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
 
     clientSocket->Connect(*inAddress);
