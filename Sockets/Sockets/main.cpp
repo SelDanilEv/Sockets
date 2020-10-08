08ec4f0c08792de22ae5f060110a2f8df03273f6
mike3293 <misha5555573@gmail.com>
2020-10-08 12:49:23 +0300

    Adjust server to lan

--- Sockets/Sockets/main.cpp
+++ Sockets/Sockets/main.cpp
@@ -3,27 +3,49 @@
 int main()
 {
     SocketUtil::StaticInit();
+    TCPSocketPtr serverSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+
+    SOCKADDR_IN addr;
     SocketAddressPtr servAddr = IPAddressFactory::CreateIPv4FromString("192.168.43.112:2000");
 
     SocketAddress* inAddress = new SocketAddress(servAddr->mSockAddr);
 
-    TCPSocketPtr clientSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+    serverSocket->Bind(*inAddress);
 
-    clientSocket->Connect(*inAddress);
+    serverSocket->Listen(1);
 
-    int i = 0;
+    TCPSocketPtr sockPtr = serverSocket->Accept(*inAddress);
 
-    while (i != 5) {
+    while (true)
+    {
+        char sibuf[50],
+            sobuf[50];
 
-        char obuf[50];
-        char ibuf[50];
+        //_itoa_s(i, sobuf, 10);
 
-        _itoa_s(++i, obuf, 10);
+        sockPtr->Receive(sibuf, strlen(sibuf) + 1);
+        std::cout << "Output: " << sibuf << std::endl;
+        sockPtr->Send(sibuf, strlen(sibuf) + 1);
 
-        clientSocket->Send(obuf, strlen(obuf) + 1);
-        clientSocket->Receive(ibuf, strlen(ibuf) + 1);
-        std::cout << std::endl << ibuf << std::endl;
+        int t = 0;
+        std::cin >> t;
+        if (t == 1)
+        {
+            sockPtr = serverSocket->Accept(*inAddress);
+        }
     }
 
-    SocketUtil::CleanUp();
+
+    ////client
+    //TCPSocketPtr clientSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
+    //
+    //clientSocket->Connect(*inAddress);
+
+    //char obuf[50] = "Hello";;
+    //char ibuf[50];
+
+    //clientSocket->Send(obuf, sizeof(obuf));
+    //clientSocket->Receive(ibuf, sizeof(ibuf));
+    //std::cout << ibuf;
+
 }
