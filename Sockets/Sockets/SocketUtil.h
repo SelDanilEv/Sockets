88cff7efe2db8c53dd5a4fad505633e5c35e314d
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2020-10-15 13:13:15 +0300

    fix: uncomment

--- Sockets/Sockets/SocketUtil.h
+++ Sockets/Sockets/SocketUtil.h
@@ -21,7 +21,7 @@ public:
 		vector< TCPSocketPtr >* outExceptSet);
 
 	static UDPSocketPtr	CreateUDPSocket(SocketAddressFamily inFamily);
-	//static TCPSocketPtr	CreateTCPSocket(SocketAddressFamily inFamily);
+	static TCPSocketPtr	CreateTCPSocket(SocketAddressFamily inFamily);
 
 private:
 
