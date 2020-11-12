5b10133be33c7fdd27e67a0e5b27d5008b9b0054
mike3293 <misha5555573@gmail.com>
2020-11-12 13:04:08 +0300

    Add project Serialization

--- Sockets/Sockets/main_tcp_server.cpp
+++ Sockets/Sockets/main_tcp_server.cpp
@@ -6,33 +6,3 @@ int main()
 
     server.DoTCPLoop();
 }
-
-
-/*  SocketUtil::StaticInit();
-  TCPSocketPtr serverSocket = SocketUtil::CreateTCPSocket(SocketAddressFamily::INET);
-
-  SOCKADDR_IN addr;
-  SocketAddressPtr servAddr = IPAddressFactory::CreateIPv4FromString("192.168.43.112:2000");
-
-  SocketAddress* inAddress = new SocketAddress(servAddr->mSockAddr);
-
-  serverSocket->Bind(*inAddress);
-
-  serverSocket->Listen(1);
-
-  TCPSocketPtr sockPtr = serverSocket->Accept(*inAddress);
-
-  while (true)
-  {
-      char sibuf[50],
-          sobuf[50];
-
-      int status = sockPtr->Receive(sibuf, strlen(sibuf) + 1);
-      if (status < 0)
-      {
-          sockPtr = serverSocket->Accept(*inAddress);
-      }
-      std::cout << "Output: " << sibuf << std::endl;
-      strcat_s(sibuf, " echo");
-      sockPtr->Send(sibuf, strlen(sibuf) + 1);
-  }*/
